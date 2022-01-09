f8d86efa6c2e501c07e01eef2d5b8db6ea0f1a3b
Ashima Garg <Ashima.Ashima1@ibm.com>
2022-01-09 12:54:21 +0530

    Register manifests

--- api/v1alpha1/labeloperator_types.go
+++ api/v1alpha1/labeloperator_types.go
@@ -29,7 +29,8 @@ type LabelOperatorSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of LabelOperator. Edit labeloperator_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Foo string `json:"foo,omitempty"`
+	Label string `json:"label"`
 }
 
 // LabelOperatorStatus defines the observed state of LabelOperator
